31317a054b156e1d5dd12c86ea3c0eb628e56e61
Cindy Zhang <cixzhang@gmail.com>
2014-12-16 10:51:48 -0800

    update adds to queue, updates occur on render
    Test Plan: npm run test
    
    Reviewers: josh, danny
    
    Reviewed By: danny
    
    CC: danny, josh, diana, jason
    
    Differential Revision: https://50.18.170.14/D4422

--- spec/grapherSpec.js
+++ spec/grapherSpec.js
@@ -7,17 +7,14 @@ describe('grapher', function () {
     expect(Grapher.textures).toBeDefined();
   });
 
-  xit('can load its textures', function (done) {
+  xit('loads its textures', function (done) {
     expect(Grapher.load).toBeDefined();
     Grapher.load().then(done);
   });
 
-  it('can define new palettes', function () {
+  it('defines new palettes', function () {
     Grapher.setPalette('greyscale', palette);
     expect(Grapher.getPalette('greyscale')).toBeDefined();
-  });
-
-  it('has textures after defining a palette', function () {
     expect(Grapher.getTexture('nodes', 0xcccccc)).toBeDefined();
   });
 
@@ -74,7 +71,7 @@ describe('a grapher instance', function () {
     expect(grapher.batches.nodes).toBeDefined();
   });
 
-  it('can set and retrieve data', function () {
+  it('sets and retrieves data', function () {
     grapher.data(network);
     expect(grapher.data()).toBe(network);
   });
@@ -85,48 +82,60 @@ describe('a grapher instance', function () {
     expect(grapher.links.length).toEqual(network.links.length);
   });
 
-  it('can update the position of nodes and links', function () {
+  it('updates the position of nodes and links', function () {
     grapher.data(network);
     network.nodes[0].x = 2;
-    grapher.update();
+    grapher.update().render();
 
     expect(getNodeCenter(grapher.nodes[0])).toEqual(network.nodes[0].x);
   });
 
-  it('can update a range of nodes and links', function () {
+  it('updates a range of nodes and links', function () {
     grapher.data(network);
 
     network.nodes[0].x = 2;
     network.nodes[1].x = 5;
 
-    grapher.update('nodes', 0, 2);
+    grapher.update('nodes', 0, 2).render();
 
     expect(getNodeCenter(grapher.nodes[0])).toEqual(network.nodes[0].x);
     expect(getNodeCenter(grapher.nodes[1])).toEqual(network.nodes[1].x);
   });
 
-  it('can update specific nodes and links by index', function () {
+  it('updates specific nodes and links by an array of indices', function () {
     grapher.data(network);
 
     network.nodes[0].x = 2;
     network.nodes[1].x = 5;
 
-    grapher.update('nodes', [0, 1]);
+    grapher.update('nodes', [0, 1]).render();
 
     expect(getNodeCenter(grapher.nodes[0])).toEqual(network.nodes[0].x);
     expect(getNodeCenter(grapher.nodes[1])).toEqual(network.nodes[1].x);
+  });
+
+  it('updates nodes or links individually by index', function () {
+    expect(grapher.updateNode).toBeDefined();
+    expect(grapher.updateLink).toBeDefined();
+    grapher.data(network).render();
+
+    var n = 0;
+    network.nodes[n].x = -100;
+
+    grapher.updateNode(n).render();
+    expect(getNodeCenter(grapher.nodes[n])).toEqual(network.nodes[n].x);
   });
 
   it('updates links attached to updating nodes', function () {
-    grapher.data(network);
+    grapher.data(network).render();
 
     network.nodes[0].x = -100;
-    grapher.update('nodes', [0]);
+    grapher.update('nodes', [0]).render();
 
     expect(grapher.links[0].position.x).toEqual(network.nodes[0].x);
   });
 
-  it('can transform given a scale and tranlate', function () {
+  it('transforms', function () {
     var transform = {scale: 0.5, translate: [100, 200]};
 
     grapher.transform(transform);
@@ -136,7 +145,14 @@ describe('a grapher instance', function () {
     expect(grapher.network.position.y).toEqual(transform.translate[1]);
   });
 
-  it('can add custom mouse event handlers', function () {
+  it('resizes', function () {
+    expect(grapher.resize).toBeDefined();
+    grapher.resize(800, 600);
+    expect(grapher.renderer.width).toBe(800);
+    expect(grapher.renderer.height).toBe(600);
+  });
+
+  it('assigns custom mouse event handlers', function () {
     var handler = function () { return true; };
     grapher.on('nodes', 'mousedown', handler);
     grapher.on('nodes', 'mouseover', handler);
@@ -147,12 +163,5 @@ describe('a grapher instance', function () {
     expect(grapher.listeners['nodes']['mousedown']).toBe(handler);
     expect(grapher.listeners['nodes']['mouseover']).toBe(handler);
     expect(grapher.listeners['nodes']['mouseout']).toBe(handler);
-  });
-
-  it('can be resized', function () {
-    expect(grapher.resize).toBeDefined();
-    grapher.resize(800, 600);
-    expect(grapher.renderer.width).toBe(800);
-    expect(grapher.renderer.height).toBe(600);
   });
 });
