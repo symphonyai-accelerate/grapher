c408ffd35209224a5ecc451e628576e67cb47c54
Conor Hanrahan <conor.hanrahan@ayasdi.com>
2018-02-15 13:59:50 -0800

    Added functions to customize canvas rendering

--- build/grapher.js
+++ build/grapher.js
@@ -300,6 +300,8 @@
     init: function (o) {
       this._super(o);
       this.context = this.canvas.getContext('2d');
+      this.custom2dNodeRenderFunction = o.custom2dNodeRenderFunction;
+      this.custom2dLinkRenderFunction = o.custom2dLinkRenderFunction;
     },
 
     render: function () {
@@ -314,20 +316,31 @@
         var node = this.nodeObjects[i];
         var cx = this.transformX(node.x) * this.resolution;
         var cy = this.transformY(node.y) * this.resolution;
-        var r = node.r * this.nodeScale * this.resolution + 2;
-
-        this.context.beginPath();
-        this.context.arc(cx, cy, r, 0, 2 * Math.PI, false);
+        var r = node.r * this.nodeScale * this.resolution;
         var colorWithCorrectAlpha = node.color.slice();
         colorWithCorrectAlpha[3] = colorWithCorrectAlpha[3] / 255;
-        var borderColor = colorWithCorrectAlpha.map(function (val, i) {
-          // darken rgb, ignore alpha
-           return i === 3 ? val : Math.max(val - 140, 0);
-        });
-        this.context.strokeStyle = 'rgba(' + borderColor.join(',') + ')';
-        this.context.fillStyle = 'rgba(' + colorWithCorrectAlpha.join(',') + ')';
-        this.context.stroke();
-        this.context.fill();
+        if (this.custom2dNodeRenderFunction) {
+          this.custom2dNodeRenderFunction({
+            context: this.context,
+            node: node,
+            cx: cx,
+            cy: cy,
+            r: r,
+            color: colorWithCorrectAlpha,
+          });
+        } else {
+          this.context.beginPath();
+          this.context.arc(cx, cy, r, 0, 2 * Math.PI, false);
+          // var borderColor = colorWithCorrectAlpha.map(function (val, i) {
+          //   // darken rgb, ignore alpha
+          //   return i === 3 ? val : Math.max(val - 140, 0);
+          // });
+          // this.context.strokeStyle = 'rgba(' + borderColor.join(',') + ')';
+          this.context.fillStyle = 'rgba(' + colorWithCorrectAlpha.join(',') + ')';
+          // this.context.stroke();
+          this.context.fill();
+        }
+
       }
     },
 
@@ -338,15 +351,28 @@
         var y1 = this.transformY(link.y1) * this.resolution;
         var x2 = this.transformX(link.x2) * this.resolution;
         var y2 = this.transformY(link.y2) * this.resolution;
-
-        this.context.beginPath();
-        this.context.moveTo(x1, y1);
-        this.context.lineTo(x2, y2);
-        this.context.lineWidth = this.lineWidth * 0.5 * this.resolution;
         var colorWithCorrectAlpha = link.color.slice();
+        var lineWidth = this.lineWidth * 0.5 * this.resolution;
         colorWithCorrectAlpha[3] = colorWithCorrectAlpha[3] / 255;
-        this.context.strokeStyle = 'rgba(' + colorWithCorrectAlpha.join(',') + ')';
-        this.context.stroke();
+        if (this.custom2dLinkRenderFunction) {
+          this.custom2dLinkRenderFunction({
+            context: this.context,
+            x1: x1,
+            y1: y1,
+            x2: x2,
+            y2: y2,
+            color: colorWithCorrectAlpha,
+            lineWidth: lineWidth,
+          });
+        } else {
+          this.context.beginPath();
+          this.context.moveTo(x1, y1);
+          this.context.lineTo(x2, y2);
+          this.context.lineWidth = lineWidth;
+          this.context.strokeStyle = 'rgba(' + colorWithCorrectAlpha.join(',') + ')';
+          this.context.stroke();
+        }
+
       }
     }
   });
